703400a9edb807452edfe4d756d98ce79c5fe237
parque <pauarqueros27@gmail.com>
2022-04-22 16:49:40 +0200

    Implementades diversos metodes. #ISSUE: No funcionen operadors == i >= per metodes insereix() i existeix()

--- LlistaOrdenadaDinamica/LlistaOrdenada.cpp
+++ LlistaOrdenadaDinamica/LlistaOrdenada.cpp
@@ -1,5 +1,6 @@
 #include "LlistaOrdenada.h"
 #include <iostream>
+using namespace std;
 
 LlistaOrdenada::LlistaOrdenada()
 {
@@ -29,5 +30,33 @@ bool LlistaOrdenada::existeix(Fitxa f) const
 			trobada = p->f == f;
 		}
 		else p = p->seg;
+	}
+}
+
+void LlistaOrdenada::mostrar() const
+{
+	Node* p = a_inici;
+	while (p != NULL)
+	{
+		p->f.mostra(); cout << "   ";
+		p = p->seg;
+	}
+}
+
+void LlistaOrdenada::inserir(Fitxa f)
+{
+	Node* ant, * p, * nou;
+	bool fi_cerca = false;
+	bool trobada = false;
+	ant = NULL;
+	p = a_inici;
+	while ((p != NULL) and (not fi_cerca))
+	{
+		if (p->f >= f)
+		{
+			fi_cerca = true;
+			trobada = p->f == f;
+
+		}
 	}
 }
