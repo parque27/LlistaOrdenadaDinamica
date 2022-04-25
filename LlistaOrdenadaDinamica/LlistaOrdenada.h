ab98d1f7aed47ca5d05d97dd61a22f720d8bbea1
PAU ARQUEROS <pauarqueros27@gmail.com>
2022-04-25 13:39:40 +0200

    Posats pre i post a interficie

--- LlistaOrdenadaDinamica/LlistaOrdenada.h
+++ LlistaOrdenadaDinamica/LlistaOrdenada.h
@@ -6,22 +6,31 @@ class LlistaOrdenada
 	public:
 		// CONSTRUCTUROS
 		LlistaOrdenada();
+		// Pre: --; Post: la llista es buida
 		LlistaOrdenada(const LlistaOrdenada& o);
+		// Pre: --; Post: la llista es una copia de o
 
 		// DESTRUCTOR
 		~LlistaOrdenada();
+		// Pre: --; Post: memoria alliberada
 
 		// OPERADORS
 		LlistaOrdenada& operator= (const LlistaOrdenada& o);
-
+		// Pre: --; Post: aquesta llista es una copia de o
+	
 		// METODES CONSULTORS
 		bool es_buida() const;
+		// Pre: --; Post: retorna true si la llista es buida, false altrament
 		bool existeix(Fitxa f) const;
+		// Pre: llista no buida; Post: retorna true si f existeix a la llista, fals altrament
 		void mostrar() const;
+		// Pre: --; Post: mostra la llista per pantalla
 
 		// METODES MODIFICADORS
 		void inserir(Fitxa f);
+		// Pre: --; Post: insereix la fitxa f de forma ordenada
 		void eliminar(Fitxa f);
+		// Pre: la llista es buida; Post: es borra f de la llista
 
 	private:
 		// ESTRUCTURA DE DADES
